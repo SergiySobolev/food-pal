d6725f497e03581f2d3e0a3d6f211a563844277c
SergiySobolev <sergiy.sobolev@gmail.com>
2016-06-06 23:16:54 +0300

    base authorization server + resource client + communication via tokens

--- account-service/src/main/java/com/sbk/foodpal/account/AccountServiceApplication.java
+++ account-service/src/main/java/com/sbk/foodpal/account/AccountServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
@@ -13,6 +14,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,6 +29,7 @@ import java.util.stream.Stream;
 
 @SpringCloudApplication
 @EnableFeignClients
+@EnableResourceServer
 public class AccountServiceApplication {
 
 	public static void main(String[] args) {
